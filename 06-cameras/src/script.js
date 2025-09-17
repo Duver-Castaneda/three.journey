e4b832a5b29342fc42723de20d438d8ec4a71b50
Duver Stiven Castañeda Garcia <duverstivencastanedagarcia@MacBook-Pro-de-Duver.local>
2025-09-17 18:10:29 -0500

    cursorEvent

--- 06-cameras/src/script.js
+++ 06-cameras/src/script.js
@@ -3,6 +3,21 @@ import * as THREE from 'three'
 /**
  * Base
  */
+
+
+const cursor = {
+    x:0,
+    y:0
+}
+window.addEventListener('mousemove', (event) =>
+{
+    cursor.x = event.clientX / sizes.width - 0.5
+    cursor.y = event.clientY / sizes.height - 0.5
+
+    console.log(cursor.x)
+}
+)
+
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
 
@@ -23,8 +38,8 @@ const mesh = new THREE.Mesh(
 scene.add(mesh)
 
 // Camera
-// const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-const aspectRatio = sizes.width / sizes.height
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+/* const aspectRatio = sizes.width / sizes.height
 const camera = new THREE.OrthographicCamera(
     -1 * aspectRatio,
     1* aspectRatio,
@@ -32,10 +47,15 @@ const camera = new THREE.OrthographicCamera(
     -1,
     0.1,
     100
-)
-camera.position.x = 2
-camera.position.y = 2
-camera.position.z = 2
+)*/
+
+
+
+
+
+//camera.position.x = 2
+//camera.position.y = 2
+camera.position.z = 3
 camera.lookAt(mesh.position)
 scene.add(camera)
 
@@ -53,7 +73,7 @@ const tick = () =>
     const elapsedTime = clock.getElapsedTime()
 
     // Update objects
-    mesh.rotation.y = elapsedTime;
+    //mesh.rotation.y = elapsedTime;
 
     // Render
     renderer.render(scene, camera)
