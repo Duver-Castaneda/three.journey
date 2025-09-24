e56cb737c1d64909172854c7c5e3796cf2970e2a
Duver Stiven Castañeda Garcia <duverstivencastanedagarcia@MacBook-Pro-de-Duver.local>
2025-09-23 21:12:07 -0500

    add meshphysicalmaterial with his gui

--- 11-materials/src/script.js
+++ 11-materials/src/script.js
@@ -59,7 +59,26 @@ gradientTexture.minFilter = THREE.NearestFilter
 gradientTexture.magFilter = THREE.NearestFilter
 gradientTexture.generateMipmaps = false
 material.gradientMap = gradientTexture*/
-const material = new THREE.MeshStandardMaterial()
+
+/*const material = new THREE.MeshStandardMaterial()
+material.metalness = 1
+material.roughness = 1
+material.map = doorcolorTexture
+material.aoMap = ambientOcclusionTexture
+material.aoMapIntensity = 1
+material.displacementMap = heightTexture
+material.displacementScale = 0.1
+material.metalnessMap = metalnessTexture
+material.roughnessMap = roughnessTexture
+material.normalMap = normalTexture
+
+material.normalScale.set(0.5,0.5)
+material.transparent = true
+material.alphaMap = alphaTexture
+gui.add(material, 'metalness').min(0).max(1).step(0.0001)
+gui.add(material, 'roughness').min(0).max(1).step(0.0001)*/
+
+const material = new THREE.MeshPhysicalMaterial()
 material.metalness = 1
 material.roughness = 1
 material.map = doorcolorTexture
@@ -76,6 +95,41 @@ material.transparent = true
 material.alphaMap = alphaTexture
 gui.add(material, 'metalness').min(0).max(1).step(0.0001)
 gui.add(material, 'roughness').min(0).max(1).step(0.0001)
+
+/*material.clearcoat = 1
+material.clearcoatRoughness = 0
+gui.add(material, 'clearcoat').min(0).max(1).step(0.0001)
+gui.add(material, 'clearcoatRoughness').min(0).max(1).step(0.0001)
+
+material.sheen = 1
+material.sheenRoughness = 0.25
+material.sheenColor.set(1,1,1)
+gui.add(material, 'sheen').min(0).max(1).step(0.0001)
+gui.add(material, 'sheenRoughness').min(0).max(1).step(0.0001)
+gui.addColor(material, 'sheenColor')*/
+
+/*material.iridescence = 1
+material.iridescenceIOR = 1
+material.iridescenceThicknessRange = [100,800]
+gui.add(material, 'iridescence').min(0).max(1).step(0.0001)
+gui.add(material, 'iridescenceIOR').min(1).max(2.333).step(0.0001)
+gui.add(material.iridescenceThicknessRange, '0').min(1).max(1000).step(1)
+gui.add(material.iridescenceThicknessRange, '1').min(1).max(1000).step(1)*/
+
+
+material.transmission = 1
+material.ior = 1.5
+material.thickness = 0.5
+gui.add(material, 'transmission').min(0).max(1).step(0.0001)
+gui.add(material, 'ior').min(1).max(10).step(0.0001)
+gui.add(material, 'thickness').min(0).max(1).step(0.0001)
+
+
+
+
+
+
+
 const sphere = new THREE.Mesh(new THREE.SphereGeometry(0.5,64,64),material)
 sphere.position.x = -1.5
 const plane = new THREE.Mesh(new THREE.PlaneGeometry(1,1,100,100),material)
