2c068963df3fbef58f691b36b90cf292ccf77103
Duver Stiven Castañeda Garcia <duverstivencastanedagarcia@MacBook-Pro-de-Duver.local>
2025-09-24 15:47:16 -0500

    center shape 1

--- 12-3d-text/src/script.js
+++ 12-3d-text/src/script.js
@@ -16,6 +16,8 @@ const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
+const axeshelper = new THREE.AxesHelper()
+scene.add(axeshelper)
 
 /**
  * Textures
@@ -37,6 +39,17 @@ fontLoader.load('/fonts/helvetiker_regular.typeface.json', (font) => {
         bevelOffset: 0,
         bevelSegments:4
     })
+
+textGeometry.computeBoundingBox()
+textGeometry.translate(
+    -(textGeometry.boundingBox.max.x - 0.02) * 0.5,
+    -(textGeometry.boundingBox.max.y - 0.02) * 0.5,
+    -(textGeometry.boundingBox.max.z - 0.03) * 0.5
+)
+
+
+
+
     const textMaterial = new THREE.MeshBasicMaterial(
     )
     textMaterial.wireframe = true
