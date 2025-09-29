fddbce2621b65e751ee7a0a34d0d43bdf4ec29dd
Duver Stiven Castañeda Garcia <duverstivencastanedagarcia@MacBook-Pro-de-Duver.local>
2025-09-28 20:44:52 -0500

    added house, added graves

--- 16-haunted-house/src/script.js
+++ 16-haunted-house/src/script.js
@@ -6,6 +6,11 @@ import GUI from 'lil-gui'
 /**
  * Base
  */
+
+
+
+
+
 // Debug
 const gui = new GUI()
 
@@ -19,10 +24,12 @@ const scene = new THREE.Scene()
  * House
  */
 
+const textureLoader = new THREE.TextureLoader()
+const alphaMapTexture = textureLoader.load('./floor/alpha.jpg')
 
 const floor = new THREE.Mesh(
     new THREE.PlaneGeometry(20,20),
-    new THREE.MeshStandardMaterial()
+    new THREE.MeshStandardMaterial({alphaMap: alphaMapTexture, transparent:true})
 )
 floor.rotation.x = - Math.PI * 0.5
 scene.add(floor)
@@ -34,17 +41,81 @@ const walls = new THREE.Mesh(
    new THREE.BoxGeometry(4,2.5,4),
     new THREE.MeshStandardMaterial()
 )
+walls.position.y = 1.25
 house.add(walls)
+
+const roof = new THREE.Mesh( new THREE.ConeGeometry(3.5,1.5,4), 
+new THREE.MeshStandardMaterial())
+roof.position.y = 2.5 + 0.75
+roof.rotation.y = Math.PI * 0.25
+house.add(roof)
+
+const door = new THREE.Mesh(
+    new THREE.PlaneGeometry(2.2,2.2),
+    new THREE.MeshStandardMaterial()
+)
+door.position.y = 1
+door.position.z = 2 + 0.01
+house.add(door)
+
+const bushGeometry = new THREE.SphereGeometry(1,16,16)
+const bushMaterial = new THREE.MeshStandardMaterial()
+
+const bush1 = new THREE.Mesh(bushGeometry,bushMaterial)
+bush1.scale.setScalar(0.5)
+bush1.position.set(0.8,0.2,2.2)
+
+
+const bush2 = new THREE.Mesh(bushGeometry,bushMaterial)
+bush2.scale.set(0.25,0.25,0.25)
+bush2.position.set(1.4,0.1,2.1)
+
+
+const bush3 = new THREE.Mesh(bushGeometry,bushMaterial)
+bush3.scale.set(0.4,0.4,0.4)
+bush3.position.set(- 0.8,0.1,2.2)
+
+
+const bush4 = new THREE.Mesh(bushGeometry,bushMaterial)
+bush4.scale.set(0.15,0.15,0.15)
+bush4.position.set(-1,0.05,2.6)
+house.add(bush1,bush2,bush3,bush4)
+
+//graves 
+const graveGeometry = new THREE.BoxGeometry(0.6,0.8,0.2)
+const graveMaterial = new THREE.MeshStandardMaterial()
+
+const graves = new THREE.Group()
+scene.add(graves)
+for(let i = 0; i < 30; i++){
+
+const angle = Math.random() * Math.PI * 2
+const radius = 3 + Math.random() * 4
+const x = Math.sin(angle) * radius
+const z = Math.cos(angle) * radius
+
+
+
+const grave = new THREE.Mesh(graveGeometry,graveMaterial)
+grave.position.x = x
+grave.position.y = Math.random() * 0.4
+grave.position.z = z
+grave.rotation.x = (Math.random() - 0.5) * 0.4
+grave.rotation.y = (Math.random() - 0.5) * 0.4
+grave.rotation.z = (Math.random() - 0.5) * 0.4
+
+graves.add(grave)
+}
 
 
 
 
 // Temporary sphere
-const sphere = new THREE.Mesh(
+/*const sphere = new THREE.Mesh(
     new THREE.SphereGeometry(1, 32, 32),
     new THREE.MeshStandardMaterial({ roughness: 0.7 })
 )
-scene.add(sphere)
+scene.add(sphere)*/
 
 /**
  * Lights
