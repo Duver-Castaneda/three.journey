025a4a16bce36b0949b1b0b4d7b052e5ff7516c4
Duver Stiven Castañeda Garcia <duverstivencastanedagarcia@MacBook-Pro-de-Duver.local>
2025-10-06 20:26:31 -0500

    added scroll, particles, positions, rotations, movement of geometries in relation to the mouse

--- 19-scroll-based-animation/src/script.js
+++ 19-scroll-based-animation/src/script.js
@@ -40,6 +40,9 @@ grandientTexture.magFilter = THREE.NearestFilter
 
 grandientTexture.colorSpace = THREE.SRGBColorSpace
 const material = new THREE.MeshToonMaterial({color: parameters.materialColor, gradientMap: grandientTexture})
+const objectsDistance = 4
+
+
 
 const mesh1 = new THREE.Mesh(new THREE.TorusGeometry(1,0.4,16,60), 
 material)
@@ -48,7 +51,38 @@ material)
 const mesh3 = new THREE.Mesh(new THREE.TorusKnotGeometry(0.8,0.35,100,16), 
 material)
 
+mesh1.position.y = -objectsDistance * 0
+mesh2.position.y = -objectsDistance * 1
+mesh3.position.y = -objectsDistance * 2
+
+mesh1.position.x = 2
+mesh2.position.x = -2
+mesh3.position.x = 2
+const sectionMeshes = [mesh1,mesh2,mesh3]
+
 scene.add(mesh1,mesh2,mesh3)
+
+const particlesCount = 200
+const positions = new Float32Array(particlesCount * 3)
+
+for(let i = 0; i < particlesCount; i++){
+    positions[i * 3 + 0] = (Math.random() - 0.5) * 10
+    positions[i * 3 + 1] = objectsDistance * 0.5 - Math.random() * objectsDistance * sectionMeshes.length
+    positions[i * 3 + 2] = (Math.random() - 0.5) * 10
+}
+
+const particlesGeometry = new THREE.BufferGeometry()
+particlesGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
+
+const particlesMaterial = new THREE.PointsMaterial({
+    color:parameters.materialColor,
+    sizeAttenuation: true,
+    size: 0.03
+})
+
+const particles = new THREE.Points(particlesGeometry, particlesMaterial)
+scene.add(particles)
+
 
 const directionalLight = new THREE.DirectionalLight('#ffffff', 3)
 directionalLight.position.set(1,1,0)
@@ -75,15 +109,23 @@ window.addEventListener('resize', () =>
     // Update renderer
     renderer.setSize(sizes.width, sizes.height)
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+
+
 })
 
 /**
  * Camera
  */
 // Base camera
+
+const cameraGroup = new THREE.Group()
+scene.add(cameraGroup)
+
+
 const camera = new THREE.PerspectiveCamera(35, sizes.width / sizes.height, 0.1, 100)
 camera.position.z = 6
-scene.add(camera)
+cameraGroup.add(camera)
 
 /**
  * Renderer
@@ -95,14 +137,51 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
+
+let scrollY = window.scrollY
+
+window.addEventListener('scroll', () => {
+    scrollY = window.scrollY
+    
+})
+
+
+
+
+const cursor = {}
+cursor.x = 0
+cursor.y = 0
+
+
+window.addEventListener('mousemove', (event) => {
+    cursor.x = event.clientX / sizes.width - 0.5
+    cursor.y = event.clientY / sizes.height - 0.5
+
+})
+
+
+
 /**
  * Animate
  */
 const clock = new THREE.Clock()
-
+let previousTime = 0
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+    const deltaTime = elapsedTime - previousTime
+    previousTime = elapsedTime
+
+camera.position.y = -scrollY / sizes.height * objectsDistance
+
+const parallaxX = cursor.x
+const parallaxY = -cursor.y
+cameraGroup.position.x += (parallaxX - cameraGroup.position.x) * 5 * deltaTime
+cameraGroup.position.y += (parallaxY - cameraGroup.position.y) * 5 * deltaTime
+    for(const mesh of sectionMeshes) {
+        mesh.rotation.x = elapsedTime * 0.1
+        mesh.rotation.y = elapsedTime * 0.12
+    }
 
     // Render
     renderer.render(scene, camera)
