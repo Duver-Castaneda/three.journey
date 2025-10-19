a61a8f74a8c4dc467f10f49853c779bcc143f0c6
Duver Stiven Castañeda Garcia <duverstivencastanedagarcia@MacBook-Pro-de-Duver.local>
2025-10-18 21:49:37 -0500

    modules explanation

---
+++ 26-code-structuring-for-bigger-projects/src/test.js
@@ -0,0 +1,18 @@
+// export default () => {
+// console.log('hello modules')
+// } 
+
+// export default {
+//     hello: 'modules'
+// }
+
+const oneThing = {
+    hello: 'modules'
+}
+
+const anotherThing = () => {
+console.log('hi')
+}
+    
+
+export {oneThing, anotherThing}
